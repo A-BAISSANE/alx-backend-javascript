47e8fdc7c744a092d920cdc9f0c94a5e33b15029
BAISSANE <baissanee@gmail.com>
2023-11-28 11:26:47 -0800

    Updates

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+// Use const to instantiate variables in taskFirst
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+// getLast function remains unchanged
+
+// Use let to instantiate variables in taskNext
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
